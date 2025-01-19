df0acb57f65ab6234534b53143b00b53520d1cf5
Pontus Hultman <pontus@griegconnect.com>
2025-01-19 10:51:40 +0100

    fix animatio

--- src/modules/home/components/HomeTitle.tsx
+++ src/modules/home/components/HomeTitle.tsx
@@ -59,10 +59,6 @@ export const HomeTitle = () => {
           maxWidth: "400px",
           height: "auto",
           marginY: -2,
-          "--time-anim": "8s", // Total duration (includes 4s pause)
-          "--delay-anim": "0s",
-          animation: `${glitchAnim} var(--time-anim) linear infinite`,
-          animationDelay: "calc(var(--delay-anim) + var(--time-anim) * 0.2)"
         }}
       />
       <Box pt={1}>
